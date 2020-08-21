694f6b0d4b7555e77e0d977eb4120fa245a1048a
WinterGirl0025 <lixiaohan@whut.edu.cn>
2020-08-21 18:49:35 +0800

    8-21

---
+++ lixiaohan/test/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/test',
+    name: 'Test',
+    component: () => import('../views/Test.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ lixiaohan/test/src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container class="all_container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style  scoped>
+.all_container{
+    color: aliceblue;
+    height: 100%;
+}
+.el-header {
+  background-color: #000;
+}
+.el-aside {
+  background-color: rgb(38, 33, 68);
+}
+.el-main {
+  background-color: rgb(156, 156, 156);
+}
+</style>
