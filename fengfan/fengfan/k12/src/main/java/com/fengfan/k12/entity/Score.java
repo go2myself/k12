5d0f87d51adb00e0b0f264402c17855e9ecaf436
冯帆 <48177121+fengfan1994@users.noreply.github.com>
2020-08-25 10:55:19 +0800

    8.25
    8.25

---
+++ fengfan/fengfan/k12/src/main/java/com/fengfan/k12/entity/Score.java
@@ -0,0 +1,22 @@
+package com.fengfan.k12.entity;
+
+public class Score {
+    int uid;
+    int score;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ fengfan/fengfan/k12/src/test/java/com/fengfan/k12/K12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengfan.k12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
